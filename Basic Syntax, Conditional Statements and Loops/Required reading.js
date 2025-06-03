b013a0e079c1ca2f82814677187cf5e9869eaa5f
Nikoleta Dimitrova <nikoletadimitrova010@gmail.com>
2025-06-03 22:47:02 +0300

    add

---
+++ Basic Syntax, Conditional Statements and Loops/Required reading.js
@@ -0,0 +1,13 @@
+function requariedReading (numOfPagesOfTheBook, pagesInOneHour, numOfDaysToReadTheBook) {
+    let pages = numOfPagesOfTheBook / numOfDaysToReadTheBook;
+
+    let result = pages / pagesInOneHour;
+
+    console.log(result);
+    
+}
+requariedReading(432,
+
+15 ,
+
+4)
